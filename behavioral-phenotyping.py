a0744ece912a3ac9488eb80be3534d51994d11ff
samouei <43193378+samouei@users.noreply.github.com>
2020-11-14 11:55:03 -0600

    video to audio

--- behavioral-phenotyping.py
+++ behavioral-phenotyping.py
@@ -3,6 +3,8 @@
 
 from pytube import YouTube 
 import os
+from pydub.utils import mediainfo
+import subprocess
 
 
 # 1.
@@ -38,11 +40,41 @@ def get_video_filepath(video_name):
     Returns: file path (string)
     Method: os module
     '''  
-    # import os
     
     video_filepath = os.path.abspath(video_name)   
     return video_filepath
 
+
+def get_video_data(video_filepath):
+    '''
+    Gets the metadata of a video.
+    Input: video file path as a string.
+    Returns: duration, channels, bit rate, sample rate (tuple)
+    Method: mediainfo from pydub.utils module
+    '''  
+    
+    video_data = mediainfo(video_filepath)
+    duration = video_data["duration"]
+    channels = video_data["channels"]
+    bit_rate = video_data["bit_rate"]
+    sample_rate = video_data["sample_rate"]
+    
+    return (duration, channels, bit_rate, sample_rate)
+
+
+def video_to_audio(video_name, audio_name):
+    '''
+    Extracts audio from video and .
+    Input: video_name.mp4 as a string.
+           audio_name.mp3 as a string
+    Returns: a task completion message (string)
+    Method: ffmpeg, subprocess module
+    '''    
+    
+    command = f"ffmpeg -i" + " " + video_name + " " + audio_name
+    subprocess.call(command, shell = True)
+    
+    print(f"'{video_name}' was converted to audio successfully.")
 
 
 
